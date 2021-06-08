5fd2677f95cfb9e4b6687f03c49815d74e8f9df1
Francesca Salvini <franci98sp@gmail.com>
2021-06-08 16:05:35 +0200

    inizio header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,20 @@
 <header>
-    Ciao sono l'header
+
+    <!-- blue section with white text -->
+    <div class="blue-section">
+        <div class="wrapper">
+            <ul>
+                <li>
+                    <a href="#">dc power visa</a>
+                </li>
+                <li>
+                    <a href="#">additional dc sites</a>
+                </li>
+            </ul>
+        </div>
+        
+    </div>
+    <!--end blue section with white text -->
+
+
 </header>
