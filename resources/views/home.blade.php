9d610227119af275e2bea9ca8f03857aa8b11254
Francesca Salvini <franci98sp@gmail.com>
2021-06-09 12:09:53 +0200

    completato main homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,7 +15,42 @@
             <div class="btn">
                 <button class="blue-e-white">load more</button>
             </div>
+
+            <div class="btn">
+                <button id="current" class="blue-e-white">current series</button>
+            </div>
             
+        </div>
+    </section>
+
+    <section>
+        <div class="blue-bg-sect">
+            <div class="wrapper">
+                <div class="white-icon">
+                    <img src="{{ asset('img/buy-comics-digital-comics.png')}}" alt="">
+                    <h5>digital comics</h5>
+                </div>
+
+                <div class="white-icon">
+                    <img src="{{ asset('img/buy-comics-merchandise.png')}}" alt="">
+                    <h5>dc merchandise</h5>
+                </div>
+
+                <div class="white-icon">
+                    <img src="{{ asset('img/buy-comics-subscriptions.png')}}" alt="">
+                    <h5>subscription</h5>
+                </div>
+
+                <div class="white-icon">
+                    <img src="{{ asset('img/buy-comics-shop-locator.png')}}" alt="">
+                    <h5>comic shop locator</h5>
+                </div>
+
+                <div class="white-icon">
+                    <img src="{{ asset('img/buy-dc-power-visa.svg')}}" alt="">
+                    <h5>dc power visa</h5>
+                </div>
+            </div>
         </div>
     </section>
 @endsection
