cd8d0c86973f69d6b8dcb35285f1a87039c0c669
Francesca Salvini <franci98sp@gmail.com>
2021-06-09 11:38:25 +0200

    impostato main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,20 @@
 
 @section('main_content')
     <section class="products-list">
-        All Comics
+        <div class="wrapper">
+            <div class="comics">
+                @foreach ($comics_array as $comic)
+                <div class="comic">
+                    <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title']}}">
+                    <h5>{{ $comic['series'] }}</h5>
+                </div>
+                @endforeach
+            </div>
+
+            <div class="btn">
+                <button class="blue-e-white">load more</button>
+            </div>
+            
+        </div>
     </section>
 @endsection
